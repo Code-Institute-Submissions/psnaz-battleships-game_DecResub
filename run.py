4ab1a2d0648b13b51d0062a95bd8af5ea9cc0a62
psnaz <psnazle@gmail.com>
2022-04-13 08:45:53 +0000

    Define size of boards and basic structure of functions

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-print("hello Petra")
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+
+
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)] # will hold our ships
+GUESS_BOARD = [[' '] * 8 for x in range(8)] # will hold our guesses: hits & misses
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def board():     # function to define the board
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass   # ask user what row and column he will want to guess the location
+
+def count_hit_ships(): #it'll count every time you have a hit & if you hit all 5 the game is over
+    pass
+
