5b55a35f7cebee43cde1e413a8d4d24fc8542a25
psnaz <psnazle@gmail.com>
2022-04-13 09:04:34 +0000

    Add function to define and print boards

--- run.py
+++ run.py
@@ -8,8 +8,13 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)] # will hold our guesses: hits & miss
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
-def board():     # function to define the board
-    pass
+def print_board(board):     # function to define the board
+    print('     A B C D E F G H')
+    print('     ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 def create_ships():
     pass
@@ -20,3 +25,6 @@ def get_ship_location():
 def count_hit_ships(): #it'll count every time you have a hit & if you hit all 5 the game is over
     pass
 
+create_ships()
+turns = 10
+#while turns > 0:
