15544cd99022a941bddd060b52d5aa087c45a1ab
psnaz <psnazle@gmail.com>
2022-04-15 15:43:21 +0000

    Add main function

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ from random import randint
 import emoji 
 
 # will hold computer generated hidden ships 
-HIDDEN_BOARD = [[' '] * 8 for x in range(8)] #originally just one space,now 2
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 # will hold player's guesses and record hits and misses
-GUESS_BOARD = [[' '] * 8 for x in range(8)] #originnally just 1 space
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
@@ -48,7 +48,7 @@ def get_ship_location():
             row = int(row)
             break
         else:
-            print(" Sorry,you've entered a wrong number.\n")
+            print(" Sorry, you've entered a wrong number.\n")
             continue
 
     while True:
@@ -56,7 +56,7 @@ def get_ship_location():
         if column and column in 'ABCDEFGH':
             break
         else:
-            print(" Sorry,you've entered a wrong letter.\n")
+            print(" Sorry, you've entered a wrong letter.\n")
             continue
     return int(row) - 1, letters_to_numbers[column]   # ask user what row and column he will want to guess the location
 
@@ -75,6 +75,49 @@ def count_hit_ships(board):
     return count
 
 
+def main():
+    """
+    Runs all the functions
+    """
+    #create_ships(HIDDEN_BOARD)
+    # print_board(HIDDEN_BOARD)  # to test if you can win or lose
+    turns = 10
+    while turns > 0:
+        print_board(GUESS_BOARD)
+        row, column = get_ship_location()
+        if GUESS_BOARD[row][column] == '-':
+            print(" You already guessed that...\U0001F641\n")
+        elif HIDDEN_BOARD[row][column] == 'X':
+            print(" Awesome!\U0001F4A5 You've hit a battleship!\U0001F6A2\n")
+            GUESS_BOARD[row][column] = 'X'
+            turns -= 1
+        else:
+            print(" Sorry, you've missed!\U0001F641\n")
+            GUESS_BOARD[row][column] = '-'
+            turns -= 1
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print(" Congrats,\U0001F44F you have sunk all the battleships!\U0001F4AA\n")
+            print(" \U0001F973 " * 10)
+            break
+        print(f" \U0001F9E8 You have {turns} shots remaining.\n")
+        if turns == 0:
+            print(" Sorry, you've run out of shots, the game's over \U0001F641")
+            break
+
+print("   " + "\U0001F30A \U0001F6A2 " * 8)
+print("\U0001F30A  WELCOME to the BATTLESHIPS GAME!\U0001F6A2")
+print("   " + "\U0001F30A \U0001F6A2 " * 8)
+print("\n")
+print(" \U0001F449 The Rules: \U0001F447\n")
+print(" \U0001F449 There are only 5 small battleships hiding on the sea\n and you have to locate them and shoot them down.\n")
+print(" \U0001F449 You have 10 shots only.\n")
+print(" \U0001F449 Every time you locate a ship and sink it,\n an 'X' will appear on the guess board in front of you.\n") 
+print(" \U0001F449 Every time you miss a shot,\n a '-' will appear on the board in front of you.\n")
+print(" \U0001F449 Once you've run out of shots or sank all 5 ships\n the game's over.\n")
+print(" Good luck!\U0001F340 LET'S PLAY!\n")
+main()
+
+"""
 create_ships(HIDDEN_BOARD)
 print_board(HIDDEN_BOARD)  #to test if you can win or lose
 turns = 10
@@ -110,3 +153,4 @@ while turns > 0:
     if turns == 0:
         print(" Sorry, you've run out of shots, the game's over \U0001F641")
         break
+"""
