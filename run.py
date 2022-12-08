8756a28aed1711ef42bb3159775ce8acf16ce3c6
psnaz <psnazle@gmail.com>
2022-12-08 20:48:32 +0000

    Reformat code and delete commented out code

--- run.py
+++ run.py
@@ -1,20 +1,17 @@
 """
-Legend
-X for placing ship and hit battleship
-' ' for available space
-'-' for missed shot
-"""
-"""
 !!! Please note that majority of this code
-comes from How to code Battleship in Python - 
-Single Player Game Youtube tutorial 
-by Knowledge Mavens !!!
+comes from How to code Battleship in Python -
+Single Player Game Youtube tutorial
+by Knowledge Mavens - see Readme.
 """
-
 from random import randint
 # import emoji module
 import emoji
 
+# Legend
+# X for placing ship and hit battleship
+# ' ' for available space
+# '-' for missed shot
 
 # will hold computer generated hidden ships
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
@@ -60,13 +57,16 @@ def create_ships(board):
 def get_ship_location():
     """
     Asks user for an input/ guess of coordinates.
-    Line 46 to 61 my code - came up together with the tutor
-    to fix the bug so that an empty string - when player hits
-    enter, wouldn't throw an error.
+    !!! Lines 64-88 my code - came up together with a tutor
+    and also my mentor to fix the bug so that an empty string 
+    - when player hits enter, wouldn't throw an error.
     """
     while True:
-        #row = input("\n \U0001F447 Please enter a ship row from 1 to 8: ")
-        row = input(emoji.emojize('\n :backhand_index_pointing_right: Please enter a ship row from 1 to 8: '))
+        row = input(
+            emoji.emojize(
+                '\n :backhand_index_pointing_right: Please enter a ship row\
+                    from 1 to 8: '
+                ))
         try:
             row = int(row)
             if row < 1 or row > 8:
@@ -77,8 +77,10 @@ def get_ship_location():
             continue
 
     while True:
-        #column = input("\U0001F447 Enter a ship column from A to H: ").upper()
-        column = input(emoji.emojize('\n :backhand_index_pointing_right: Please enter a ship column from A to H: ')).upper()
+        column = input(
+            emoji.emojize(
+                '\n :backhand_index_pointing_right: Please enter a ship column from\
+                     A to H:')).upper()
         if len(column) == 1 and column in 'ABCDEFGH':
             break
         else:
@@ -112,64 +114,81 @@ def main():
     create_ships(HIDDEN_BOARD)
     turns = 10
     while turns > 0:
-        print_board(HIDDEN_BOARD) # TO BE DELETED ONCE TESTING'S DONE!!!
+        print_board(HIDDEN_BOARD)   # TO BE DELETED ONCE TESTING'S DONE!!!
         print_board(GUESS_BOARD)
         row, column = get_ship_location()
         if GUESS_BOARD[row][column] == '-':
-            #print(" You already guessed that...\U0001F641\n")
-            print(emoji.emojize('\nYou already guessed that...:slightly_frowning_face:\n'))
+            # my code lines 122-124
+            print(
+                emoji.emojize(
+                    '\nYou already guessed that...:slightly_frowning_face:\n'))
         elif HIDDEN_BOARD[row][column] == 'X':
-            #print(" Awesome!\U0001F4A5 You've hit a battleship!\U0001F6A2\n")
-            print(emoji.emojize("\n Awesome!:collision: You've hit a battleship!:ship:\n"))
+            # my code lines 127-129
+            print(
+                emoji.emojize(
+                    "\n Awesome!:collision: You've hit a battleship!:ship:\n"))
             GUESS_BOARD[row][column] = 'X'
             turns -= 1
         else:
-            #print(" Sorry, you've missed!\U0001F641\n")
-            print(emoji.emojize("\n Sorry, you've missed!:slightly_frowning_face:\n"))
+            # my code lines 134-136
+            print(
+                emoji.emojize(
+                    "\n Sorry, you've missed! :slightly_frowning_face:\n"))
             GUESS_BOARD[row][column] = '-'
             turns -= 1
         if count_hit_ships(GUESS_BOARD) == 5:
-            #print(" Congrats,\U0001F44F you have sunk all the battleships!\n")
-            print(emoji.emojize('\n Congrats,:clapping_hands: you have sunk all the battleships!:flexed_biceps:\n'))
-            #print(" \U0001F973 " * 10)  # my code
-            print(emoji.emojize(' :partying_face: ') *10)
+            # my code lines 141-145
+            print(
+                emoji.emojize(
+                    '\n Congrats,:clapping_hands: you have sunk \
+                        all the battleships!:flexed_biceps:\n'))
+            print(emoji.emojize(' :partying_face: ') * 10)
             break
         # my code f-string
-        print(f" \U0001F9E8 You have {turns} shots remaining.\n")
+        print(
+            emoji.emojize(
+                f":firecracker: You have {turns} shots remaining.\n"))
         if turns == 0:
-            #print("Sorry, you've run out of shots, game over\U0001F641\n")
-            print(emoji.emojize("Sorry, you've run out of shots, game over:slightly_frowning_face:\n"))
-            # my code
-            #print("Here's where all the ships were hiding...\U0001F447\n")
-            print(emoji.emojize("Here's where all the ships were hiding...:backhand_index_pointing_down:\n"))
+            # my code lines 153-160
+            print(
+                emoji.emojize(
+                    "Sorry, you've run out of shots, \
+                        game over:slightly_frowning_face:\n"))
+            print(
+                emoji.emojize(
+                    "Here's where all the ships were hiding...\
+                        :backhand_index_pointing_down:\n"))
             print_board(HIDDEN_BOARD)
             break
 
 
 # welcome and rules - my code, removed from the while loop
-#print("   " + "\U0001F30A \U0001F6A2 " * 8)
 print(emoji.emojize(':water_wave: :ship: ') * 8)
-#print("\U0001F30A  WELCOME to the BATTLESHIPS GAME!\U0001F6A2")
 print(emoji.emojize(':water_wave: WELCOME to the BATTLESHIPS GAME!:ship:'))
-#print("   " + "\U0001F30A \U0001F6A2 " * 8)
 print(emoji.emojize(':water_wave: :ship: ') * 8)
 print("\n")
-#print(" \U0001F449 The Rules: \U0001F447\n")
-print(emoji.emojize(' :backhand_index_pointing_right: The Rules: :backhand_index_pointing_down:\n'))
-#print(" \U0001F449 There are only 5 small battleships hiding on")
-print(emoji.emojize(' :backhand_index_pointing_right: There are only 5 small battleships hiding on'))
-print("the sea and you have to locate them and shoot them down.\n")
-#print(" \U0001F449 You have 10 shots only.\n")
-print(emoji.emojize(' :backhand_index_pointing_right: You have 10 shots only.\n'))
-#print(" \U0001F449 Every time you locate a ship and sink it,")
-print(emoji.emojize(' :backhand_index_pointing_right: Every time you locate a ship and sink it,'))
-print(" an 'X' will appear on the guess board in front of you.\n")
-#print(" \U0001F449 Every time you miss a shot,")
-print(emoji.emojize(' :backhand_index_pointing_right: Every time you miss a shot,'))
+print(
+    emoji.emojize(
+        ':backhand_index_pointing_right: The Rules:\
+            :backhand_index_pointing_down:\n'))
+print(
+    emoji.emojize(
+        ':backhand_index_pointing_right: There are only 5 small battleships'))
+print("hiding on the sea and you have to locate them and shoot them down.\n")
+print(
+    emoji.emojize(
+        ':backhand_index_pointing_right: You have 10 shots only.\n'))
+print(
+    emoji.emojize(
+        ':backhand_index_pointing_right: Every time you locate a ship and'))
+print("sink it, an 'X' will appear on the guess board in front of you.\n")
+print(
+    emoji.emojize(
+        ':backhand_index_pointing_right: Every time you miss a shot,'))
 print("a '-' will appear on the board in front of you.\n")
-#print(" \U0001F449 Once you've run out of shots or")
-print(emoji.emojize(" :backhand_index_pointing_right: Once you've run out of shots or"))
+print(
+    emoji.emojize(
+        ":backhand_index_pointing_right: Once you've run out of shots or"))
 print("sunk all 5 ships the game's over.\n")
-#print(" Good luck!\U0001F340 LET'S PLAY!\n")
 print(emoji.emojize(" Good luck!:four_leaf_clover: LET'S PLAY!\n"))
 main()
