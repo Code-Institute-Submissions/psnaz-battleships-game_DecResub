3affa7df25651cf270d1290e301e54677aac1960
psnaz <psnazle@gmail.com>
2022-12-09 11:28:29 +0000

    Amend code attributions

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 """
 !!! Please note that majority of this code
-comes from How to code Battleship in Python -
+originally comes from How to code Battleship in Python -
 Single Player Game Youtube tutorial
-by Knowledge Mavens - see Readme.
+by Knowledge Mavens - see Readme, however was
+amended by me to fix several bugs.
 """
 from random import randint
 # import emoji module
@@ -17,7 +18,7 @@ import emoji
 HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
 # will hold player's guesses and record hits and misses
 GUESS_BOARD = [[' '] * 8 for x in range(8)]
-# will help to concatinate printed texts - MY CODE
+# will help to concatinate printed texts - MY CODE: line 21-28
 PROMPT_ROW_TEXTS = [
     '\n :backhand_index_pointing_right: Please enter a ship row',
     'from 1 to 8: '
@@ -27,7 +28,7 @@ PROMPT_COLUMN_TEXTS = [
     'A to H: '
 ]
 
-# indexing horizontal row
+# indexing horizontal row - tutorial code
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -39,7 +40,7 @@ letters_to_numbers = {
     'H': 7
     }
 
-
+# Tutorial code - line 44-53
 def print_board(board):
     """
     Defines the board size
@@ -51,7 +52,7 @@ def print_board(board):
         print(f"{row_number}|{'|'.join(row)}|")
         row_number += 1
 
-
+# Tutorial code - line 56-65
 def create_ships(board):
     """
     Computer generates 5 ships in 5 unique locations
@@ -67,9 +68,11 @@ def create_ships(board):
 def get_ship_location():
     """
     Asks user for an input/ guess of coordinates.
-    !!! Lines 64-88 my code - came up together with a tutor
-    and also my mentor to fix the bug so that an empty string
+    !!! Lines 80-87 my code - came up together with a tutor
+    to fix the bug so that an empty string
     - when player hits enter, wouldn't throw an error.
+    Amended with a help of my mentor to fix other issues -
+    eg. input of certain multiple letters eg. ab.
     """
 
     while True:
@@ -93,7 +96,7 @@ def get_ship_location():
 
     return int(row) - 1, letters_to_numbers[column]
 
-
+# Tutorial code
 def count_hit_ships(board):
     """
     Counts every time the user has a hit and
@@ -107,7 +110,7 @@ def count_hit_ships(board):
                 count += 1
     return count
 
-
+# The main ideas taken from tutorial but redone
 def main():
     """
     Runs all the functions
@@ -121,26 +124,26 @@ def main():
         print_board(GUESS_BOARD)
         row, column = get_ship_location()
         if GUESS_BOARD[row][column] == '-':
-            # my code lines 122-124
+            # my code lines 128-130
             print(
                 emoji.emojize(
                     '\nYou already guessed that...:slightly_frowning_face:\n'))
         elif HIDDEN_BOARD[row][column] == 'X':
-            # my code lines 127-129
+            # my code lines 132-135
             print(
                 emoji.emojize(
                     "\n Awesome!:collision: You've hit a battleship!:ship:\n"))
             GUESS_BOARD[row][column] = 'X'
             turns -= 1
         else:
-            # my code lines 134-136
+            # my code lines 140-142
             print(
                 emoji.emojize(
                     "\n Sorry, you've missed! :slightly_frowning_face:\n"))
             GUESS_BOARD[row][column] = '-'
             turns -= 1
         if count_hit_ships(GUESS_BOARD) == 5:
-            # my code lines 141-145
+            # my code lines 146-151
             print(
                 emoji.emojize(
                     '\n Congrats,:clapping_hands: you have sunk \
@@ -152,7 +155,7 @@ def main():
             emoji.emojize(
                 f":firecracker: You have {turns} shots remaining.\n"))
         if turns == 0:
-            # my code lines 153-160
+            # my code lines 158-166
             print(
                 emoji.emojize(
                     "Sorry, you've run out of shots, \
@@ -165,7 +168,7 @@ def main():
             break
 
 
-# welcome and rules - my code, removed from the while loop
+# Welcome and rules - my code, removed from the while loop
 print(emoji.emojize(':water_wave: :ship: ') * 8)
 print(emoji.emojize(':water_wave: WELCOME to the BATTLESHIPS GAME!:ship:'))
 print(emoji.emojize(':water_wave: :ship: ') * 8)
